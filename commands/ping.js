a2dcc018b99a5e0ddbeede9a07098cbec0f0cc23
Glitch (time-chlorinated-duke) <none>
2020-10-20 09:18:27 +0000

    💗🐛 Updated with Glitch

---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+//FIRST TEST HANDLER IS WORKING OR NOT
+module.exports = {
+  name: "ping",
+  description: "Ping Of The Bot",
+  execute(client, message) {
+    message.channel.send(`Pong :) 🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  }
+};
